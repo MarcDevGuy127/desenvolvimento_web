05baf9039232c06d4f1656f0a99601d4ecf1ec04
MarcDevGuy127 <marceloh.github@gmail.com>
2025-11-05 17:40:41 -0300

    Implementing password validator.

--- banco/php/cadastrar.php
+++ banco/php/cadastrar.php
@@ -14,9 +14,23 @@ try {
     $uf = filter_input(INPUT_POST, 'uf', FILTER_SANITIZE_SPECIAL_CHARS);
     $cidade = filter_input(INPUT_POST, 'cidade', FILTER_SANITIZE_SPECIAL_CHARS);
     $complemento = filter_input(INPUT_POST, 'bairro', FILTER_SANITIZE_SPECIAL_CHARS);
+    $senha_usuario = filter_input(INPUT_POST, 'senha_usuario', FILTER_SANITIZE_SPECIAL_CHARS);
+
+    #if (sizeof($senha_usuario ) >= 8 || sizeof($senha_usuario ) <= 12) {
+        # code...
+    #}
+
+    //impedir senhas fora do permitido
+    if (sizeof($senha_usuario ) < 8 || sizeof($senha_usuario ) > 12) {
+        echo '<h1>senha inválida!</h1>';
+        header('Location: index.html');
+        exit();
+    }
+
+    $hash = password_hash($senha_usuario, PASSWORD_DEFAULT);
 
     // Inserir dados usando prepared statement
-    $stmt = $pdo->prepare("INSERT INTO usuarios (nome, email, telefone, pais, uf, cidade, complemento) VALUES (:nome, :email)");
+    $stmt = $pdo->prepare("INSERT INTO usuarios (nome, email, telefone, pais, uf, cidade, complemento, senha_usuario) VALUES (:nome, :email)");
     $stmt->bindParam(':nome', $nome);
     $stmt->bindParam(':email', $email);
     $stmt->bindParam(':telefone', $telefone);
@@ -24,6 +38,8 @@ try {
     $stmt->bindParam(':uf', $uf);
     $stmt->bindParam(':cidade', $cidade);
     $stmt->bindParam(':complemento', $complemento);
+    $stmt->bindParam(':senha_usuario', $senha_usuario);
+
 
     if ($stmt->execute()) {
         echo "Cadastro realizado com sucesso!<br>";
