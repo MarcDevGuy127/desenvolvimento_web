9c2b18822d6c3a14837eea341c0c4036df9c5e8a
MarcDevGuy127 <marceloh.github@gmail.com>
2025-11-06 12:06:02 -0300

    chore - Add bd constants.

---
+++ banco/php/config.php
@@ -0,0 +1,25 @@
+<?php
+// arquivo php destinado para configurações.
+// Definindo constantes de conexão com o banco
+define('DB_HOST', 'localhost');      // especificando servidor local XAMPP
+define('DB_USER', 'root');           // especificando usuário padrão do MySQL no XAMPP
+define('DB_PASS', '');               // especificando senha de acesso ao banco padrão (vazia)
+define('DB_NAME', 'plataforma_banco');      // especificando nome do banco
+
+/*
+// configurações de constantes extras
+
+// Configurações de email
+define('SMTP_HOST', '');
+define('SMTP_PORT', 'numeroaqui');
+define('SMTP_USER', '');
+define('SMTP_PASS', '');
+define('SMTP_FROM', '');
+define('SMTP_FROM_NAME', 'nomeaqui');
+
+// Configurações gerais
+define('SITE_URL', 'https://site.com.br');
+define('UPLOAD_PATH', 'pasta'); */
+
+
+?>
